274ea45587f068675d0339e380b9767be0371f28
Yiming Mao <yimingmao@yimings-macbook.local>
2016-10-01 21:04:36 +0800

    Initial Commit

---
+++ AlgorithmHw1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmHw1
+//
+//  Created by Yiming Mao on 01/10/2016.
+//  Copyright © 2016 Yiming Mao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
